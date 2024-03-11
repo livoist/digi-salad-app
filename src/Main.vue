22a368a8df6fdde373bf372e164d10373c7f52a4
lai Ben <waterblue104@gmail.com>
2024-03-11 20:52:52 +0800

    add Main comp and update App comp

---
+++ src/Main.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Intro from './components/Intro.vue'
+import About from './components/About.vue'
+import Services from './components/Services.vue'
+import Experience from './components/Experience.vue'
+import Projects from './components/Projects.vue'
+
+</script>
+
+<template>
+<Intro></Intro>
+<About></About>
+<Services></Services>
+<Experience></Experience>
+<Projects></Projects>
+</template>
