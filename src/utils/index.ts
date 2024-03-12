a40c88e94d25d8778b0f7adcb857e0ce32af1329
lai Ben <waterblue104@gmail.com>
2024-03-12 15:05:58 +0800

    update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,3 @@
 export const delay = (ms: number) => new Promise(resolve => setTimeout(() => resolve(true), ms));
+export const handleImgName = (name: string) => name.toLowerCase().split(" ").join("-");
+export const iconUrl = (name: string) => new URL(`../assets/servicesIcon/${name}.png`, import.meta.url).href;
