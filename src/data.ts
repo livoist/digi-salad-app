50a42d4591ad59ce1c66026967aa172ce2d9b951
lai Ben <waterblue104@gmail.com>
2024-03-12 15:06:11 +0800

    add data file

---
+++ src/data.ts
@@ -0,0 +1,43 @@
+interface ServicesContent {
+  name: string,
+  link: string
+}
+
+export const servicesContent: ServicesContent[] = [
+  {
+    name: "UX DESIGN",
+    link: ""
+  },
+  {
+    name: "UI DESIGN",
+    link: ""
+  },
+  {
+    name: "WEBSITE DEVELOPMENT",
+    link: ""
+  },
+  {
+    name: "MOBILE APP DEVELOPMENT",
+    link: ""
+  },
+  {
+    name: "ECOMMERCE",
+    link: ""
+  },
+  {
+    name: "CUSTOMER LOYALTY",
+    link: ""
+  },
+  {
+    name: "DIGITAL TRANSFORMATION",
+    link: ""
+  },
+  {
+    name: "DIGITAL MARKETING",
+    link: ""
+  },
+  {
+    name: "BRANDING",
+    link: ""
+  }
+];
